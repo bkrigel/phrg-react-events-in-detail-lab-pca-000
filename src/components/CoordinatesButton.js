5bcb6e4effc6dbc32236f2b023142bdf7517f144
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-10-02 22:49:29 -0400

    event.persist() confuses me still a bit

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+  receiveCoordinates = (event) => {
+    this.props.onReceiveCoordinates(
+      [event.pageX, event.pageY]
+    )
+  }
+
+  render() {
+    return (
+      <button onClick={this.receiveCoordinates} />
+    )
+  }
+}
+
+export default CoordinatesButton;
